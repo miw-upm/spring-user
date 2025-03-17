138983300298e1ddd0ea81602de0d603954a96cd
JBernal <j.bernal@upm.es>
2025-03-17 17:25:51 +0100

    removed authenticationProvider

--- src/main/java/es/upm/api/configurations/UserAuthConfig.java
+++ src/main/java/es/upm/api/configurations/UserAuthConfig.java
@@ -5,10 +5,6 @@ import es.upm.api.data.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -36,14 +32,6 @@ public class UserAuthConfig { // Authentication with user:password
                     .authorities(new SimpleGrantedAuthority(user.getScope().scopeValue()))
                     .build();
         };
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
     }
 
     @Bean
