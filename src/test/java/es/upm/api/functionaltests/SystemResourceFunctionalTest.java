e268f66f89e98762115c2cfa50064d703331d230
JBernal <j.bernal@upm.es>
2025-03-18 01:40:31 +0100

    refactored resources test

--- src/test/java/es/upm/api/functionaltests/SystemResourceFunctionalTest.java
+++ src/test/java/es/upm/api/functionaltests/SystemResourceFunctionalTest.java
@@ -1,12 +1,10 @@
 package es.upm.api.functionaltests;
 
 import es.upm.api.resources.SystemResource;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -16,23 +14,12 @@ import static org.springframework.http.HttpStatus.OK;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
 class SystemResourceFunctionalTest {
-
-    @LocalServerPort
-    private int port;
-
     @Autowired
     private TestRestTemplate restTemplate;
 
-    private String baseUrl;
-
-    @BeforeEach
-    void setup() {
-        this.baseUrl = "http://localhost:" + port;
-    }
-
     @Test
     void testReadBadge() {
-        String url = this.baseUrl + SystemResource.VERSION_BADGE;
+        String url = SystemResource.VERSION_BADGE;
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(OK);
@@ -43,7 +30,7 @@ class SystemResourceFunctionalTest {
 
     @Test
     void testReadInfo() {
-        ResponseEntity<String> response = restTemplate.getForEntity(this.baseUrl, String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(OK);
         assertThat(response.getBody())
--- src/test/java/es/upm/api/functionaltests/UserResourceFunctionalTest.java
+++ src/test/java/es/upm/api/functionaltests/UserResourceFunctionalTest.java
@@ -1,13 +1,13 @@
 package es.upm.api.functionaltests;
 
+import es.upm.api.data.entities.Scope;
 import es.upm.api.resources.view.UserDto;
 import lombok.extern.log4j.Log4j2;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,28 +19,21 @@ import static es.upm.api.resources.UserResource.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Log4j2
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class UserResourceFunctionalTest {
-    @LocalServerPort
-    private int port;
+    private final TestRestTemplate testRestTemplate;
+    private final HttpEntityFactory httpEntityFactory;
 
     @Autowired
-    private TestRestTemplate testRestTemplate;
-
-    @Autowired
-    private RestTestService restTestService;
-
-    private String baseUrl;
-
-    @BeforeEach
-    void setup() {
-        baseUrl = "http://localhost:" + port + USERS;
+    UserResourceFunctionalTest(@Value("${miw.oauth2.api-client-id}") String apiClientId, @Value("${miw.oauth2.api-client-secret}") String apiClientSecret, TestRestTemplate testRestTemplate) {
+        this.testRestTemplate = testRestTemplate;
+        this.httpEntityFactory = new HttpEntityFactory(testRestTemplate, apiClientId, apiClientSecret);
     }
 
     @Test
     void testReadUser() {
-        ResponseEntity<UserDto> response = testRestTemplate.exchange(baseUrl + MOBILE_ID, HttpMethod.GET, this.restTestService.loginAdmin(), UserDto.class, "6");
+        ResponseEntity<UserDto> response = testRestTemplate.exchange(USERS + MOBILE_ID, HttpMethod.GET, this.httpEntityFactory.create(Scope.ADMIN), UserDto.class, "6");
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isNotNull();
         assertThat(response.getBody().getFirstName()).isEqualTo("admin");
@@ -48,47 +41,47 @@ class UserResourceFunctionalTest {
 
     @Test
     void testReadUserNotFound() {
-        ResponseEntity<UserDto> response = testRestTemplate.exchange(baseUrl + MOBILE_ID, HttpMethod.GET, restTestService.loginAdmin(), UserDto.class, "666000666");
+        ResponseEntity<UserDto> response = testRestTemplate.exchange(USERS + MOBILE_ID, HttpMethod.GET, httpEntityFactory.create(Scope.ADMIN), UserDto.class, "666000666");
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
     @Test
     void testReadUserUnauthorized() {
-        ResponseEntity<UserDto> response = testRestTemplate.exchange(baseUrl + MOBILE_ID, HttpMethod.GET, restTestService.createHttpEntity(), UserDto.class, "6");
+        ResponseEntity<UserDto> response = testRestTemplate.exchange(USERS + MOBILE_ID, HttpMethod.GET, httpEntityFactory.create(), UserDto.class, "6");
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
     }
 
     @Test
     void testCreateUserWithAdmin() {
         UserDto userDto = UserDto.builder().mobile("666001666").firstName("daemon").build();
-        ResponseEntity<Void> response = testRestTemplate.exchange(baseUrl, HttpMethod.POST, restTestService.loginAdmin(userDto), Void.class);
+        ResponseEntity<Void> response = testRestTemplate.exchange(USERS, HttpMethod.POST, httpEntityFactory.create(userDto, Scope.ADMIN), Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
     void testCreateUserConflict() {
         UserDto userDto = UserDto.builder().mobile("666666000").firstName("daemon").build();
-        ResponseEntity<Void> response = testRestTemplate.exchange(baseUrl, HttpMethod.POST, restTestService.loginAdmin(userDto), Void.class);
+        ResponseEntity<Void> response = testRestTemplate.exchange(USERS, HttpMethod.POST, httpEntityFactory.create(userDto, Scope.ADMIN), Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
     }
 
     @Test
     void testCreateUserBadNumber() {
         UserDto userDto = UserDto.builder().mobile("1").firstName("daemon").build();
-        ResponseEntity<Void> response = testRestTemplate.exchange(baseUrl, HttpMethod.POST, restTestService.loginAdmin(userDto), Void.class);
+        ResponseEntity<Void> response = testRestTemplate.exchange(USERS, HttpMethod.POST, httpEntityFactory.create(userDto, Scope.ADMIN), Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 
     @Test
     void testCreateUserWithoutNumber() {
         UserDto userDto = UserDto.builder().mobile(null).firstName("daemon").build();
-        ResponseEntity<Void> response = testRestTemplate.exchange(baseUrl, HttpMethod.POST, restTestService.loginAdmin(userDto), Void.class);
+        ResponseEntity<Void> response = testRestTemplate.exchange(USERS, HttpMethod.POST, httpEntityFactory.create(userDto, Scope.ADMIN), Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 
     @Test
     void testReadOperator() {
-        ResponseEntity<UserDto[]> response = testRestTemplate.exchange(baseUrl, HttpMethod.GET, restTestService.loginOperator(), UserDto[].class);
+        ResponseEntity<UserDto[]> response = testRestTemplate.exchange(USERS, HttpMethod.GET, httpEntityFactory.create(Scope.OPERATOR), UserDto[].class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isNotNull();
         assertThat(Arrays.stream(response.getBody()).map(UserDto::getFirstName).toList())
@@ -98,7 +91,7 @@ class UserResourceFunctionalTest {
 
     @Test
     void testSearch() {
-        ResponseEntity<UserDto[]> response = testRestTemplate.exchange(baseUrl + SEARCH + "?dni=c", HttpMethod.GET, restTestService.loginManager(), UserDto[].class);
+        ResponseEntity<UserDto[]> response = testRestTemplate.exchange(USERS + SEARCH + "?dni=c", HttpMethod.GET, httpEntityFactory.create(Scope.MANAGER), UserDto[].class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isNotNull();
         assertThat(Arrays.stream(response.getBody()).map(UserDto::getFirstName).toList())
@@ -108,7 +101,7 @@ class UserResourceFunctionalTest {
 
     @Test
     void testSearchDoesNotContainNull() {
-        ResponseEntity<String> response = testRestTemplate.exchange(baseUrl + SEARCH, HttpMethod.GET, restTestService.loginManager(), String.class);
+        ResponseEntity<String> response = testRestTemplate.exchange(USERS + SEARCH, HttpMethod.GET, httpEntityFactory.create(Scope.MANAGER), String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).doesNotContain("null");
         log.debug("json: {}", response.getBody());
